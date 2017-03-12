067f183bb6a68f4460a934e4770b5aefab908af0
Rishabh04-02 <rishabh0402@gmail.com>
2017-03-12 20:54:22 +0530

    modifications, features
    navbar added, pages created

--- js/main.js
+++ js/main.js
@@ -105,3 +105,17 @@ $(".fancybox").fancybox({
         }
     }
 });
+
+//menu navigation customizations
+$(function() {
+                $('#navigation a').stop().animate({'marginLeft':'-85px'},1000);
+
+                $('#navigation > li').hover(
+                    function () {
+                        $('a',$(this)).stop().animate({'marginLeft':'-2px'},200);
+                    },
+                    function () {
+                        $('a',$(this)).stop().animate({'marginLeft':'-85px'},200);
+                    }
+                );
+            });
---
+++ navigation.php
@@ -0,0 +1,10 @@
+<!-- This is the navigation file -->
+<ul id="navigation">
+            <li class="home"><a href="index.php" title="Home"></a></li>
+            <li class="about"><a href="about.php" title="About"></a></li>
+            <li class="contact"><a href="contact.php" title="Contact"></a></li>
+            <li class="projects"><a href="projects.php" title="Projects"></a></li>
+            <li class="events"><a href="events.php" title="Events"></a></li>
+            <li class="members"><a href="members.php" title="Members"></a></li>
+            <li class="latest"><a href="profile.php" title="Latest"></a></li>
+</ul>
