6346b5fb4c28462d71dabe0328c7271c763bc617
Rishabh04-02 <rishabh0402@gmail.com>
2017-03-26 15:29:28 +0530

    tooltips added on header

--- header.php
+++ header.php
@@ -26,26 +26,26 @@
                 <nav class="collapse navbar-collapse navbar-right" role="navigation">
                     <div class="main-menu">
                         <ul class="nav navbar-nav navbar-right">
-                            <li><a href="index.php">Home</a></li>
-                            <li><a href="about.php">About</a></li>
-                            <li><a href="contact.php">Contact</a></li>
-                            <li><a href="projects.php">Projects</a></li>
-                            <li><a href="events.php">Events</a></li>
+                            <li><a href="index.php" class="tooltip-show" data-toggle="tooltip" title="Team .EXE">Home</a></li>
+                            <li><a href="about.php" class="tooltip-show" data-toggle="tooltip" title="About - Team .EXE">About</a></li>
+                            <li><a href="contact.php" class="tooltip-show" data-toggle="tooltip" title="Contact - Team .EXE">Contact</a></li>
+                            <li><a href="projects.php" class="tooltip-show" data-toggle="tooltip" title="View Projects by Team .EXE">Projects</a></li>
+                            <li><a href="events.php" class="tooltip-show" data-toggle="tooltip" title="View Events by Team .EXE">Events</a></li>
 
 <?php 
 include_once('user_session.php');
  
    if(!isset($_SESSION['login_user']))
    {
-       echo '<li><a href="login.php">Sign In</a></li>';
+       echo '<li><a href="login.php" class="tooltip-show" data-toggle="tooltip" title="Sign In">Sign In</a></li>';
    }
    //if user session is set then redirecting user to profile
    else
    {
-       echo '<li><a href="profile.php">';
+       echo '<li><a href="profile.php" class="tooltip-show" data-toggle="tooltip" title="Visit Profile">';
        echo $session_usr;
        echo '</a></li>'; 
-       echo '<li><a href="logout.php">Logout</a>';
+       echo '<li><a href="logout.php" class="tooltip-show" data-toggle="tooltip" title="Logout">Logout</a>';
    }
 ?>
                             </li>
