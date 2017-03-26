0cc740ed46dc3ee84f8ac8d6e7dc706920ee29c9
Rishabh04-02 <rishabh0402@gmail.com>
2017-03-26 16:01:22 +0530

    footer fixed

--- footer.php
+++ footer.php
@@ -20,23 +20,30 @@
                 //updating the new value in database
                 $qryu=mysqli_query($link,"UPDATE hits SET cnt='$out' WHERE nam='$namee'");
         ?>
-<footer class="footer">
-    <div class="container">
-    <div class="col-md-8">
-    <p class="copyright">&copy; : <span>2017</span> | Designed, Developed & Hosted by <b><a href="index.php" title="Team EXE official">Team .EXE</a></b></p>
-    </div>
-    <div class="col-md-4">
-                       <!--  Social Media -->
-                            
-    <a href="https://www.youtube.com/channel/UCTIpvLaM1G-uUsthgCDauKw" target="_blank" title="Subscribe us on Youtube" class="Youtube">
-        <img class="ft" src="images/youtube.svg" /><!--<i class="ion-social-youtube"></i>-->
-    </a>
-    <a href="https://www.instagram.com/teamexenith/" target="_blank" title="Follow on Instagram" class="Instagram">
-        <img class="ft" src="images/insta.svg" /><!--<i class="ion-social-instagram"></i>-->
-    </a>                         
-    <a href="https://www.facebook.com/teamexe/" target="_blank" title="Like us on Facebook" class="Facebook">
-        <img class="ft" src="images/fb.svg" /><!--<i class="ion-social-facebook"></i>-->
-    </a>
-    </div>
-    </div>
-</footer>
+        <footer id="footer">
+            <div class="container">
+                <div class="col-md-8">
+                    <p class="copyright">&copy;: <span>2017</span> | Designed, Developed & Hosted by <b><a href="index.php" title="Team EXE official website">Team .EXE</a></b></p>
+                </div>
+                <div class="col-md-4">
+                    <!-- Social Media -->
+                    <ul class="social">
+                        <li>
+                            <a href="https://www.facebook.com/teamexe/" target="_blank" title="Like us on Facebook" class="Facebook">
+                                <i class="ion-social-facebook"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.youtube.com/channel/UCTIpvLaM1G-uUsthgCDauKw" target="_blank" title="Subscribe us on Youtube" class="Youtube">
+                                <i class="ion-social-youtube"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.instagram.com/teamexenith/" target="_blank" title="Follow on Instagram" class="Instagram">
+                                <i class="ion-social-instagram"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer> <!-- /#footer -->
