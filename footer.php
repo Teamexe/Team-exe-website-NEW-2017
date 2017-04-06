5981c88f469616ff5989e6b8078c8e92f36b02f4
Rishabh04-02 <rishabh0402@gmail.com>
2017-04-06 13:23:25 +0530

    footer love, color fixed

--- footer.php
+++ footer.php
@@ -23,7 +23,7 @@
         <footer id="footer">
             <div class="container">
                 <div class="col-md-8">
-                    <p class="copyright">&copy;: <span>2017</span> | Designed, Developed & Hosted with <span style="color: red; font-size: 25px;">&hearts;</span> by <b><a href="index.php" title="Team EXE official website">Team .EXE</a></b></p>
+                    <p class="copyright">&copy;: <span>2017</span> | Designed, Developed & Hosted with <span style="color: #FF0000; font-size: 25px;">&hearts;</span> by <b><a href="index.php" title="Team EXE official website">Team .EXE</a></b></p>
                 </div>
                 <div class="col-md-4">
                     <!-- Social Media -->
